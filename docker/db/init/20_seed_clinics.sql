5c29db05fdf59d0f49521982ed56100596cd0efe
Dariusz Mędrala <dariusz.medrala@student.pk.edu.pl>
2026-06-02 17:04:00 +0200

    feat(db): add clinic join code for registration

--- docker/db/init/20_seed_clinics.sql
+++ docker/db/init/20_seed_clinics.sql
@@ -1,3 +1,3 @@
-INSERT INTO clinics (name, address) VALUES
-('Przychodnia Centrum', 'ul. Główna 1, 00-001 Warszawa'),
-('Lecznica Wesoła Łapa', 'ul. Polna 5, 30-002 Kraków');
+INSERT INTO clinics (name, address, join_code) VALUES
+('Przychodnia Centrum', 'ul. Główna 1, 00-001 Warszawa', 'CENTRUM-2024'),
+('Lecznica Wesoła Łapa', 'ul. Polna 5, 30-002 Kraków', 'WESOLA-2024');
